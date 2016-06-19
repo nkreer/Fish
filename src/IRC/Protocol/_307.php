74bc3d582cfd2bb0b405765744aa4ae6f9dcb451
Niklas <nkreer@users.noreply.github.com>
2016-06-19 19:49:55 +0200

    Prevent identification message from showing multiple times after the user has already been identified.

--- src/IRC/Protocol/_307.php
+++ src/IRC/Protocol/_307.php
@@ -43,12 +43,14 @@ class _307 implements ProtocolCommand{
             $ev = new UserIdentifyEvent($user);
             $connection->getEventHandler()->callEvent($ev);
             if(!$ev->isCancelled()){
+                if($user->identified === AuthenticationStatus::UNCHECKED){
+                    if($config->getData("authentication_message")["enabled"] === true){
+                        $user->sendNotice($config->getData("authentication_message")["message"]);
+                    }
+                }
                 $user->identified = AuthenticationStatus::IDENTIFIED;
                 $config = IRC::getInstance()->getConfig();
                 $connection->getScheduler()->scheduleDelayedTask(new UpdateAuthenticationStatusTask($user, $connection), $config->getData("authentication_ttl"));
-                if($config->getData("authentication_message")["enabled"] === true){
-                    $user->sendNotice($config->getData("authentication_message")["message"]);
-                }
             }
         }
     }
